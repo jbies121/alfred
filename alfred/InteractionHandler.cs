27ed814a02b0944efe33fbd94a331f4ca239381a
jbies121 <15245768+jbies121@users.noreply.github.com>
2022-11-26 22:59:47 -0600

    create test event by name

--- alfred/InteractionHandler.cs
+++ alfred/InteractionHandler.cs
@@ -11,29 +11,23 @@ namespace alfred
         private readonly DiscordSocketClient _client;
         private readonly InteractionService _commands;
         private readonly IServiceProvider _services;
-        private readonly IConfigurationRoot _config;
-        private ulong _guildId;
 
         public InteractionHandler(
             DiscordSocketClient client,
             InteractionService commands,
-            IServiceProvider services,
-            IConfigurationRoot config
+            IServiceProvider services
         )
         {
             _client = client;
             _commands = commands;
             _services = services;
-            _config = config;
-            _guildId = UInt64.Parse(_config["testGuild"]);
         }
 
         public async Task InitializeAsync()
         {
-            Console.WriteLine(_guildId);
             await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
             _client.InteractionCreated += HandleInteraction;
-            _client.SlashCommandExecuted += HandleSlashCommand;
+            //_client.SlashCommandExecuted += HandleSlashCommand;
         }
 
         private async Task HandleInteraction(SocketInteraction arg)
@@ -43,24 +37,6 @@ namespace alfred
                 // https://discord.com/developers/docs/interactions/receiving-and-responding#security-and-authorization
                 var ctx = new SocketInteractionContext(_client, arg);
                 await _commands.ExecuteCommandAsync(ctx, _services);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
-        }
-
-        private async Task HandleSlashCommand(SocketSlashCommand arg)
-        {
-            try
-            {
-                switch(arg.CommandName)
-                {
-                    case "session":
-                        var guild = _client.GetGuild(_guildId);
-                        await guild.CreateEventAsync("test event", DateTimeOffset.UtcNow.AddDays(1),  GuildScheduledEventType.External, endTime: DateTimeOffset.UtcNow.AddDays(2), location: "Space");
-                        break;
-                }
             }
             catch (Exception ex)
             {
--- alfred/Modules/InteractionModule.cs
+++ alfred/Modules/InteractionModule.cs
@@ -1,11 +1,26 @@
 using Discord.Interactions;
 using Discord;
 using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
 
 namespace alfred.Modules
 {
     public class InteractionModule : InteractionModuleBase<SocketInteractionContext>
     {
+        private readonly DiscordSocketClient _client;
+        private readonly IConfigurationRoot _config;
+        private ulong _guildId;
+
+        public InteractionModule(
+            DiscordSocketClient client,
+            IConfigurationRoot config
+        )
+        {
+            _client = client;
+            _config = config;
+            _guildId = UInt64.Parse(_config["testGuild"]);
+        }
+        
         [SlashCommand("slap", "Remind Alfred who the real Bat- I mean Man of the house is..")]
         public async Task HandleSlapCommand()
         {
@@ -24,6 +39,9 @@ namespace alfred.Modules
         public async Task HandleSessionCommand(string name, DateTime start)
         {
                 string Response = "Event Received: " + name + " - " + start;
+
+                var guild = _client.GetGuild(_guildId);
+                await guild.CreateEventAsync(name, DateTimeOffset.UtcNow.AddDays(1),  GuildScheduledEventType.External, endTime: DateTimeOffset.UtcNow.AddDays(2), location: "Space");
                 await RespondAsync(Response);
         }
     }
