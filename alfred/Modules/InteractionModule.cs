af8549789c3b3a782d51234c5b09bfdb60b9c40e
jbies121 <15245768+jbies121@users.noreply.github.com>
2022-12-04 01:58:34 -0600

    Start and End time codes

--- alfred/Modules/InteractionModule.cs
+++ alfred/Modules/InteractionModule.cs
@@ -40,35 +40,31 @@ namespace alfred.Modules
         [SlashCommand("session", "Alfred, I want to play with my Bat-friends.")]
         public async Task HandleSessionCommand(string Name, DateTime Start, TimeSpan Duration, string Location, string? Description = null)
         {
-                try
-                {
-                    // Calculate End time and create the scheduled event
-                    DateTime End = Start + Duration;
-                    var guildEvent = await _guild.CreateEventAsync(Name, Start,  GuildScheduledEventType.External, endTime: End, location: Location, description: Description);
-                    // Get URL of scheduled event
-                    string eventURL = "https://discord.com/events/" + _guildId + "/" + guildEvent.Id;
-                    // Build Embed Response
-                    string Response = "New Session: " + Name;
-                    EmbedBuilder embed = new EmbedBuilder
-                    {
-                        // Embed property can be set within object initializer
-                        Title = Response,
-                    };
-                        // Or with methods
-                    embed.WithAuthor(Context.Client.CurrentUser)
-                        .WithFooter(footer => footer.Text = Context.Client.CurrentUser.ToString())
-                        .WithColor(Color.Green)
-                        .WithDescription(Description)
-                        .WithUrl(eventURL)
-                        .WithCurrentTimestamp();
-
-                    //Your embed needs to be built before it is able to be sent
-                    await RespondAsync(embed: embed.Build());
-                }
-                catch (Exception ex)
-                {
-                    await RespondAsync(ex.ToString());
-                }
+            // Calculate End time and create the scheduled event
+            DateTimeOffset End = Start + Duration;
+            DateTimeOffset StartOffset = Start;
+            var guildEvent = await _guild.CreateEventAsync(Name, Start,  GuildScheduledEventType.External, endTime: End, location: Location, description: Description);
+            // Get URL of scheduled event
+            string eventURL = "https://discord.com/events/" + _guildId + "/" + guildEvent.Id;
+            // Build Embed Response
+            string Response = "New Session: " + Name;
+            string startTimeCode = "<t:" + StartOffset.ToUnixTimeSeconds().ToString() + ":R>";
+            string endTimeCode = "<t:" + End.ToUnixTimeSeconds().ToString() + ":R>";
+            EmbedBuilder embed = new EmbedBuilder
+            {
+                // Embed property can be set within object initializer
+                Title = Response,
+                ThumbnailUrl = "https://cdn.discordapp.com/attachments/214118660896194561/1040866115024326816/Buckaroo_Banzai_hogwarts_221c63f9-d003-42c8-a279-26169085a861.png"
+            };
+                // Or with methods
+            embed.WithFooter(footer => footer.Text = "Event Scheduled")
+                .WithColor(Color.Green)
+                .WithDescription(Description)
+                .AddField("Starts",startTimeCode)
+                .AddField("Ends",endTimeCode)
+                .WithUrl(eventURL);
+            //Your embed needs to be built before it is able to be sent
+            await RespondAsync(embed: embed.Build());
         }
     }
 }
