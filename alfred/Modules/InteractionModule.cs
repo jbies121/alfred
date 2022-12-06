23cfe1cd0a78e7520bdd19f6acc46a7ec77b054d
jbies121 <15245768+jbies121@users.noreply.github.com>
2022-12-05 19:11:27 -0600

    Ask for timezone

--- alfred/Modules/InteractionModule.cs
+++ alfred/Modules/InteractionModule.cs
@@ -24,34 +24,46 @@ namespace alfred.Modules
         }
 
         [SlashCommand("session", "Alfred, I want to play with my Bat-friends.")]
-        public async Task HandleSessionCommand(string Name, DateTime Start, TimeSpan Duration, string Location, string? Description = null)
+        public async Task HandleSessionCommand(string Name, DateTime Start, String Timezone, TimeSpan Duration, string Location, string? Description = null)
         {
-            // Calculate End time and create the scheduled event
-            DateTimeOffset End = Start + Duration;
-            DateTimeOffset StartOffset = Start;
-            var guildEvent = await _guild.CreateEventAsync(Name, Start,  GuildScheduledEventType.External, endTime: End, location: Location, description: Description);
-            // Get URL of scheduled event
-            string eventURL = "https://discord.com/events/" + _guildId + "/" + guildEvent.Id;
-            // Build Embed Response
-            string Response = "New Session: " + Name;
-            string startTimeCode = "<t:" + StartOffset.ToUnixTimeSeconds().ToString() + ":R>";
-            string endTimeCode = "<t:" + End.ToUnixTimeSeconds().ToString() + ":R>";
-            EmbedBuilder embed = new EmbedBuilder
+            try 
             {
-                // Embed property can be set within object initializer
-                Title = Response,
-                ThumbnailUrl = guildEvent.Guild.IconUrl
-            };
-                // Or with methods
-            embed.WithFooter(footer => footer.Text = "Event Scheduled")
-                .WithAuthor(Context.User)
-                .WithColor(Color.Green)
-                .WithDescription(Description)
-                .AddField("Starts",startTimeCode)
-                .AddField("Ends",endTimeCode)
-                .WithUrl(eventURL);
-            //Your embed needs to be built before it is able to be sent
-            await RespondAsync(embed: embed.Build());
+                TimeZoneInfo UserTimezone = TimeZoneInfo.FindSystemTimeZoneById(Timezone);
+                DateTimeOffset ConvertedStart = TimeZoneInfo.ConvertTime(Start, UserTimezone);
+                // Calculate End time and create the scheduled event
+                DateTimeOffset End = ConvertedStart + Duration;
+                var guildEvent = await _guild.CreateEventAsync(Name, ConvertedStart,  GuildScheduledEventType.External, endTime: End, location: Location, description: Description);
+                // Get URL of scheduled event
+                string eventURL = "https://discord.com/events/" + _guildId + "/" + guildEvent.Id;
+                // Build Embed Response
+                string Response = "New Session: " + Name;
+                string startTimeCode = "<t:" + ConvertedStart.ToUnixTimeSeconds().ToString() + ":R>";
+                string endTimeCode = "<t:" + End.ToUnixTimeSeconds().ToString() + ":R>";
+                EmbedBuilder embed = new EmbedBuilder
+                {
+                    // Embed property can be set within object initializer
+                    Title = Response,
+                    ThumbnailUrl = guildEvent.Guild.IconUrl
+                };
+                    // Or with methods
+                embed.WithFooter(footer => footer.Text = "Event Scheduled")
+                    .WithAuthor(Context.User)
+                    .WithColor(Color.Green)
+                    .WithDescription(Description)
+                    .AddField("Starts",startTimeCode)
+                    .AddField("Ends",endTimeCode)
+                    .WithUrl(eventURL);
+                //Your embed needs to be built before it is able to be sent
+                await RespondAsync(embed: embed.Build());
+            }
+            catch (TimeZoneNotFoundException) 
+            {
+                await RespondAsync("Unable to retrieve the time zone.");
+            }
+            catch (InvalidTimeZoneException) 
+            {
+                await RespondAsync("Unable to retrieve the time zone.");
+            }
         }
     }
 }
