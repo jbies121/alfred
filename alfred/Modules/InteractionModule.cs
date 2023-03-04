0898e6473c1c7e71ebaf9311efcc561beea052d1
jbies121 <15245768+jbies121@users.noreply.github.com>
2023-03-04 16:57:14 -0600

    fix errors

--- alfred/Modules/InteractionModule.cs
+++ alfred/Modules/InteractionModule.cs
@@ -6,9 +6,16 @@ namespace alfred.Modules
     public class InteractionModule : InteractionModuleBase<SocketInteractionContext>
     {
         [SlashCommand("session", "Alfred, I want to play with my Bat-friends.")]
-        public async Task HandleSessionCommand([Summary(description: "Name of your session")]string Name, [Summary(description: "ex. 'now', '6:00 PM', or '2023-01-31 13:00'")]string Start, [Summary(description: "ex. 'Eastern Standard Time', 'EST'")]string Timezone, [Summary(description: "ex. '1:00' is 1 hour.")]string Duration, [Summary(description: "Where is the session located?")]string Location, [Summary(description: "Any other details to mention?")]string? Description = null)
+        public async Task HandleSessionCommand(
+            [Summary(description: "Name of your session")] string Name,
+            [Summary(description: "ex. 'now', '6:00 PM', or '2023-01-31 13:00'")] string Start,
+            [Summary(description: "ex. 'Eastern Standard Time', 'EST'")] string Timezone,
+            [Summary(description: "ex. '1:00' is 1 hour.")] string Duration,
+            [Summary(description: "Where is the session located?")] string Location,
+            [Summary(description: "Any other details to mention?")] string? Description = null
+        )
         {
-            try 
+            try
             {
                 // Parse and convert datetime, handle 'now' as an input
                 TimeZoneInfo UserTimezone = TimeZoneInfo.FindSystemTimeZoneById(Timezone);
@@ -21,32 +28,57 @@ namespace alfred.Modules
                 else
                 {
                     DateTime ParsedStart = DateTime.Parse(Start);
-                    ConvertedStart = TimeZoneInfo.ConvertTime(ParsedStart, UserTimezone, TimeZoneInfo.Utc);
+                    ConvertedStart = TimeZoneInfo.ConvertTime(
+                        ParsedStart,
+                        UserTimezone,
+                        TimeZoneInfo.Utc
+                    );
                 }
 
                 TimeSpan DurationSpan = TimeSpan.Parse(Duration);
                 // If user did not supply a date and the converted time is >2 days ahead of the source timezone, jump back a day.
                 if (!(Start.Contains("-") || Start.Contains("/")))
                 {
-                    var UserTimezoneNow = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, UserTimezone);
+                    var UserTimezoneNow = TimeZoneInfo.ConvertTimeFromUtc(
+                        DateTime.UtcNow,
+                        UserTimezone
+                    );
                     if (UserTimezoneNow.Day - ConvertedStart.Day == -2)
                     {
                         ConvertedStart = ConvertedStart - TimeSpan.FromDays(1);
                     }
                 }
                 // Handle Start time from the past
-                DateTimeOffset GuildStartTime = ConvertedStart < DateTime.Now ? DateTimeOffset.Now.AddMinutes(1) : ConvertedStart;
+                DateTimeOffset GuildStartTime =
+                    ConvertedStart < DateTime.Now
+                        ? DateTimeOffset.Now.AddMinutes(1)
+                        : ConvertedStart;
                 // Calculate End time and create the scheduled event
                 DateTimeOffset End = ConvertedStart + DurationSpan;
-                var guildEvent = await Context.Guild.CreateEventAsync(Name, GuildStartTime,  GuildScheduledEventType.External, endTime: End, location: Location, description: Description);
+                var guildEvent = await Context.Guild.CreateEventAsync(
+                    Name,
+                    GuildStartTime,
+                    GuildScheduledEventType.External,
+                    endTime: End,
+                    location: Location,
+                    description: Description
+                );
                 // Get URL of scheduled event
-                string eventURL = "https://discord.com/events/" + Context.Guild.Id + "/" + guildEvent.Id;
+                string eventURL =
+                    "https://discord.com/events/" + Context.Guild.Id + "/" + guildEvent.Id;
                 // Get server profile or default user profile
-                string AuthorName = Context.Guild.GetUser(Context.User.Id).Nickname != null ? Context.Guild.GetUser(Context.User.Id).Nickname : Context.User.Username;
-                string AuthorIcon = Context.Guild.GetUser(Context.User.Id).GetGuildAvatarUrl() != null ? Context.Guild.GetUser(Context.User.Id).GetGuildAvatarUrl() : Context.User.GetAvatarUrl();
+                string AuthorName =
+                    Context.Guild.GetUser(Context.User.Id).Nickname != null
+                        ? Context.Guild.GetUser(Context.User.Id).Nickname
+                        : Context.User.Username;
+                string AuthorIcon =
+                    Context.Guild.GetUser(Context.User.Id).GetGuildAvatarUrl() != null
+                        ? Context.Guild.GetUser(Context.User.Id).GetGuildAvatarUrl()
+                        : Context.User.GetAvatarUrl();
                 // Build Embed Response
                 string Response = "New Session: " + Name;
-                string startTimeCode = "<t:" + ConvertedStart.ToUnixTimeSeconds().ToString() + ":F>";
+                string startTimeCode =
+                    "<t:" + ConvertedStart.ToUnixTimeSeconds().ToString() + ":F>";
                 string endTimeCode = "<t:" + End.ToUnixTimeSeconds().ToString() + ":R>";
                 EmbedBuilder embed = new EmbedBuilder
                 {
@@ -54,63 +86,68 @@ namespace alfred.Modules
                     Title = Response,
                     ThumbnailUrl = guildEvent.Guild.IconUrl
                 };
-                    // Or with methods
-                embed.WithFooter(footer => footer.Text = Location)
+                // Or with methods
+                embed
+                    .WithFooter(footer => footer.Text = Location)
                     .WithAuthor(AuthorName, AuthorIcon)
                     .WithColor(Color.Blue)
                     .WithDescription(Description)
-                    .AddField("Starts",startTimeCode)
-                    .AddField("Ends",endTimeCode)
+                    .AddField("Starts", startTimeCode)
+                    .AddField("Ends", endTimeCode)
                     .WithUrl(eventURL);
                 //Send embed with mention
-                await RespondAsync(Context.Guild.EveryoneRole.Mention,embed: embed.Build());
+                await RespondAsync(Context.Guild.EveryoneRole.Mention, embed: embed.Build());
             }
-            catch (TimeZoneNotFoundException) 
+            catch (TimeZoneNotFoundException)
             {
                 await RespondAsync("Unable to retrieve the time zone.", ephemeral: true);
             }
-            catch (InvalidTimeZoneException) 
+            catch (InvalidTimeZoneException)
             {
                 await RespondAsync("Unable to retrieve the time zone.", ephemeral: true);
             }
             catch (ArgumentNullException)
             {
-                await RespondAsync("DateTime needs to be in the future, try again.", ephemeral: true);
+                await RespondAsync(
+                    "DateTime needs to be in the future, try again.",
+                    ephemeral: true
+                );
             }
             catch (FormatException)
             {
                 await RespondAsync("Duration not in a valid format.", ephemeral: true);
             }
         }
-        
-        public enum Series {"F1","F3"};
+
         [SlashCommand("penalties", "Alfred, who has the most Bat-Penalties?")]
-        public async Task HandleSessionCommand([Summary(description: "Which Series? F1 or F3")]enum Series)
+        public async Task HandlePenaltiesCommand(
+            [Summary(description: "Which Series? F1 or F3")] string Series
+        )
         {
-            string Description = "Current penalty point standings:"
-            var res = File
-                .ReadLines("penalty.txt")
-                .Select((v, i) => new {Index = i, Value = v})
+            string Description = "Current penalty point standings:";
+            Dictionary<string, string> res = File.ReadLines("penalty.txt")
+                .Select((v, i) => new { Index = i, Value = v })
                 .GroupBy(p => p.Index / 2)
                 .ToDictionary(g => g.First().Value, g => g.Last().Value);
 
             EmbedBuilder embed = new EmbedBuilder
-                {
-                    // Embed property can be set within object initializer
-                    Title = "Penalty Point Standings",
-                    ThumbnailUrl = guildEvent.Guild.IconUrl
-                };
-                    // Or with methods
-                embed.WithFooter(footer => footer.Text = Location)
-                    .WithColor(Color.Red)
-                    .WithDescription(Description)
-                
-                foreach (string driver in res)
-                {
-                    embed.AddField(driver, driver.Value)
-                }
-                //Send embed with mention
-                await RespondAsync(Context.Guild.EveryoneRole.Mention,embed: embed.Build());
+            {
+                // Embed property can be set within object initializer
+                Title = "Penalty Point Standings",
+                ThumbnailUrl = Context.Guild.IconUrl
+            };
+            // Or with methods
+            embed
+                .WithFooter(footer => footer.Text = "This is a footer")
+                .WithColor(Color.Red)
+                .WithDescription(Description);
+
+            foreach (KeyValuePair<string, string> driver in res)
+            {
+                embed.AddField(driver.Key, driver.Value);
+            }
+            //Send embed with mention
+            await RespondAsync(Context.Guild.EveryoneRole.Mention, embed: embed.Build());
         }
     }
 }
